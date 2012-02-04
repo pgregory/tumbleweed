f77b5de2d46bace152e68b9bdf22256171d1ff38
Paul Gregory <pgregory@aqsis.org>
2012-02-04 22:47:06 +0000

    Minor changes to the event loop integration.

--- events/events.c
+++ events/events.c
@@ -1,20 +1,75 @@
 #define EV_STANDALONE 1
 #include "ev.c"
 
-static void timer_cb(struct ev_loop *loop, ev_timer *w, int revents)
-{
-  printf("Tick!\n");
-  ev_timer_again(loop, w);
-}
-
-void ev_test()
+ev_timer* create_timer(void(*cb)(EV_P_ struct ev_timer* w, int revents))
 {
   struct ev_loop *loop = EV_DEFAULT;
 
-  ev_timer timer_hit;
-  ev_init(&timer_hit, timer_cb);
-  timer_hit.repeat = 1.0;
-  ev_timer_again(loop, &timer_hit);
+  ev_timer* timer = calloc(1, sizeof(ev_timer));
+  ev_init(timer, cb);
+  timer->repeat = 1.0;
 
-  ev_run(loop, 0);
+  return timer;
+}
+
+void delete_timer(ev_timer* timer)
+{
+  struct ev_loop *loop = EV_DEFAULT;
+
+  ev_timer_stop(loop, timer);
+  free(timer);
+}
+
+void start_timer(ev_timer* timer)
+{
+  struct ev_loop *loop = EV_DEFAULT;
+  
+  ev_timer_again(loop, timer);
+}
+
+void stop_timer(ev_timer* timer)
+{
+  struct ev_loop *loop = EV_DEFAULT;
+  
+  ev_timer_stop(loop, timer);
+}
+
+ev_io* create_io(void(*cb)(EV_P_ struct ev_io* w, int revents), int fd)
+{
+  struct ev_loop *loop = EV_DEFAULT;
+
+  ev_io* io = calloc(1, sizeof(ev_io));
+  ev_init(io, cb);
+  ev_io_set(io, fd, EV_READ);
+
+  return io;
+}
+
+void delete_io(ev_io* io)
+{
+  struct ev_loop *loop = EV_DEFAULT;
+
+  ev_io_stop(loop, io);
+  free(io);
+}
+
+void start_io(ev_io* io)
+{
+  struct ev_loop *loop = EV_DEFAULT;
+  
+  ev_io_start(loop, io);
+}
+
+void stop_io(ev_io* io)
+{
+  struct ev_loop *loop = EV_DEFAULT;
+  
+  ev_io_stop(loop, io);
+}
+
+void run(int flags)
+{
+  struct ev_loop *loop = EV_DEFAULT;
+
+  ev_run(loop, flags);
 }
