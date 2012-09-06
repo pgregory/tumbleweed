2e67201e69fb51a98cdb5465cc96c24427ce4ae6
Paul Gregory <paul.gregory@marmalademail.com>
2012-09-06 12:10:41 +0100

    Fix exports in SFML module to work on Win32.

--- sfml/sfml.cpp
+++ sfml/sfml.cpp
@@ -1,10 +1,16 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
+#if defined WIN32
+#define EXPORT extern "C" __declspec(dllexport)
+#else
+#define EXPORT extern "C"
+#endif
+
 sf::Texture* texture;
 sf::Sprite* sprite;
 
-extern "C" sf::RenderWindow* windowCreate(int width, int height, int bitsPerPixel, const char* name)
+extern "C" EXPORT sf::RenderWindow* windowCreate(int width, int height, int bitsPerPixel, const char* name)
 {
     // Create the window
     sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode(width, height, bitsPerPixel), name);
@@ -12,29 +18,29 @@ extern "C" sf::RenderWindow* windowCreate(int width, int height, int bitsPerPixe
     return window;
 }
 
-extern "C" void windowDisplay(sf::RenderWindow* window)
+EXPORT void windowDisplay(sf::RenderWindow* window)
 {
     window->display();
 }
 
-extern "C" void windowClear(sf::RenderWindow* window)
+EXPORT void windowClear(sf::RenderWindow* window)
 {
     window->clear();
 }
 
-extern "C" void windowDraw(sf::RenderWindow* window, sf::Drawable* drawable)
+EXPORT void windowDraw(sf::RenderWindow* window, sf::Drawable* drawable)
 {
     window->draw(*drawable);
 }
 
-extern "C" sf::Texture* textureCreate()
+EXPORT sf::Texture* textureCreate()
 {
     sf::Texture* texture = new sf::Texture();
 
     return texture;
 }
 
-extern "C" int textureLoadFromFile(sf::Texture* texture, const char* file)
+EXPORT int textureLoadFromFile(sf::Texture* texture, const char* file)
 {
     if(!texture->loadFromFile(file))
         return 0;
@@ -42,41 +48,41 @@ extern "C" int textureLoadFromFile(sf::Texture* texture, const char* file)
         return 1;
 }
 
-extern "C" sf::Sprite* spriteCreate()
+EXPORT sf::Sprite* spriteCreate()
 {
     sf::Sprite* sprite = new sf::Sprite();
 
     return sprite;
 }
 
-extern "C" void spriteSetTexture(sf::Sprite* sprite, const sf::Texture* texture)
+EXPORT void spriteSetTexture(sf::Sprite* sprite, const sf::Texture* texture)
 {
     sprite->setTexture(*texture);
 }
 
-extern "C" void spriteSetPosition(sf::Sprite* sprite, float xpos, float ypos)
+EXPORT void spriteSetPosition(sf::Sprite* sprite, float xpos, float ypos)
 {
     sprite->setPosition(xpos, ypos);
 }
 
-extern "C" void spriteMove(sf::Sprite* sprite, float xoffset, float yoffset)
+EXPORT void spriteMove(sf::Sprite* sprite, float xoffset, float yoffset)
 {
     sprite->move(xoffset, yoffset);
 }
 
-extern "C" void spriteRotate(sf::Sprite* sprite, float angle)
+EXPORT void spriteRotate(sf::Sprite* sprite, float angle)
 {
     sprite->rotate(angle);
 }
 
-extern "C" sf::Font* fontCreate()
+EXPORT sf::Font* fontCreate()
 {
     sf::Font* font = new sf::Font();
 
     return font;
 }
 
-extern "C" int fontLoadFromFile(sf::Font* font, const char* file)
+EXPORT int fontLoadFromFile(sf::Font* font, const char* file)
 {
     if(!font->loadFromFile(file))
         return 0;
@@ -84,30 +90,30 @@ extern "C" int fontLoadFromFile(sf::Font* font, const char* file)
         return 1;
 }
 
-extern "C" sf::Text* textCreate()
+EXPORT sf::Text* textCreate()
 {
     sf::Text* text = new sf::Text();
 
     return text;
 }
 
-extern "C" void textSetString(sf::Text* text, const char* string)
+EXPORT void textSetString(sf::Text* text, const char* string)
 {
     text->setString(string);
 }
 
 
-extern "C" void textSetFont(sf::Text* text, sf::Font* font)
+EXPORT void textSetFont(sf::Text* text, sf::Font* font)
 {
     text->setFont(*font);
 }
 
-extern "C" void textSetCharacterSize(sf::Text* text, int size)
+EXPORT void textSetCharacterSize(sf::Text* text, int size)
 {
     text->setCharacterSize(size);
 }
 
-extern "C" int windowPollEvent(sf::RenderWindow* window)
+EXPORT int windowPollEvent(sf::RenderWindow* window)
 {
     sf::Event event;
     bool result = window->pollEvent(event);
