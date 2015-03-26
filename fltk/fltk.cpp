3ba6a8e09c3f2fe52ff237b6a9bb0a07a56845cf
Paul Gregory <aqsis1@gmail.com>
2015-03-26 00:11:01 +0000

    Update FLTK API coverage.
     * Add Button
     * Add Input
     * Add 'wait' support
     * Add 'callback' support.

--- fltk/fltk.cpp
+++ fltk/fltk.cpp
@@ -1,5 +1,7 @@
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Input.H>
 #include <FL/Fl_Box.H>
 
 #if defined WIN32
@@ -8,17 +10,41 @@
 #define EXPORT extern "C"
 #endif
 
+EXPORT int Fl_wait(double time)
+{
+  return Fl::wait(0.0);
+}
+
+
 EXPORT Fl_Window* createWindow(int width, int height) {
   Fl_Window* window = new Fl_Window(width, height);
-  Fl_Box *box = new Fl_Box(20,40,300,100,"Hello, World!");
-  box->box(FL_UP_BOX);
-  box->labelfont(FL_BOLD+FL_ITALIC);
-  box->labelsize(36);
-  box->labeltype(FL_SHADOW_LABEL);
-  window->end();
-  window->show();
-
-  printf("Created a new window\n");
 
   return window;
 }
+
+EXPORT void show(Fl_Window* window) {
+  window->show();
+}
+
+EXPORT Fl_Button* createButton(int x, int y, int w, int h, const char* label)
+{
+  Fl_Button* button = new Fl_Button(x, y, w, h);
+  button->copy_label(label);
+
+  return button;
+}
+
+EXPORT Fl_Input* Fl_createInput(int x, int y, int w, int h, const char* label)
+{
+  Fl_Input* input = new Fl_Input(x, y, w, h);
+  input->copy_label(label);
+
+  return input;
+}
+
+
+EXPORT void Fl_setCallback(Fl_Widget* widget, Fl_Callback0* cb)
+{
+  widget->callback(cb);
+}
+
